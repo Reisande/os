00e10233aee44fb25e395e368ecd5fd432e661a8
Misha Ryabko <michael0ryabko@gmail.com>
2023-07-02 15:19:32 -0700

    Added unit test runner

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
 #![no_std]
 #![no_main]
+#![reexport_test_harness_main = "test_main"]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 
 use core::panic::PanicInfo;
 
 mod vga_buffer;
 
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
+}
+
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	use core::fmt::Write;
-
-	vga_buffer::WRITER.lock().write_str("Hello again").unwrap();
-    write!(vga_buffer::WRITER.lock(), ", some numbers: {} {}", 42, 1.337).unwrap();
+	println!("Hello world!");
+	
+	#[cfg(test)]
+    test_main();
 	
     loop {
 	}
